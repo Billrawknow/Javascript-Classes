ad5cca6dc035c7935292661c28048b18c6790555
Billrawknow <bildadronoh@gmail.com>
2023-09-10 19:22:22 +0300

    Creating class library and adding methods

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ constructor(title, author, ISBN, availability = true) {
     this.availability = availability;
   }
 }
+class Library {
+    constructor() {
+      this.books = [];
+    }
+  
+    addBook(book) {
+      this.books.push(book);
+    }
+  
+    removeBook(ISBN) {
+      this.books = this.books.filter((book) => book.ISBN !== ISBN);
+    }
+  
+    displayAvailableBooks() {
+      console.log("Available Books:");
+      this.books.forEach((book, index) => {
+        if (book.availability) {
+          console.log(`${index+1}. ${book.title} by ${book.author}`);
+        }
+      });
+    }
+  }
