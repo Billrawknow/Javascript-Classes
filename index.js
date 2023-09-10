8667213e0ce4eca2af71f5bba1d9fef4606d5d49
Billrawknow <bildadronoh@gmail.com>
2023-09-10 19:18:00 +0300

    creating a class that will store the properties of a book.

---
+++ index.js
@@ -0,0 +1,8 @@
+class Book {
+constructor(title, author, ISBN, availability = true) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.availability = availability;
+  }
+}
